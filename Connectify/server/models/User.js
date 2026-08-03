26ed1acd68e3ca4a1ec34208f29071209914aaac
alisha-tech21 <alisha.itseries@gmail.com>
2026-08-03 18:24:35 +0500

    create login, register, email verification, and forget password logic

--- Connectify/server/models/User.js
+++ Connectify/server/models/User.js
@@ -42,15 +42,17 @@ const userSchema = new mongoose.Schema(
     },
     resetPasswordToken: String,
     resetPasswordExpire: Date,
+
+    emailVerificationToken: String,
+    emailVerificationExpire: Date,
   },
   { timestamps: true },
 );
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
   const salt = await bcrypt.genSalt(12);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
